99411ba70a89aee4afc9a78d01a5cd25279f3d72
samixtures <samibajwa@csu.fullerton.edu>
2021-12-29 12:03:43 -0800

    Initial Commit

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+array.shift();
+array.sort();
+array.push("Kiwi");
+array.shift(); //-> array.splice(0,1);
+array.reverse();
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+array2[1][1][0];
