1639dd01d72aa527fd34845a5f1c9fa7db28a82d
samixtures <samibajwa@csu.fullerton.edu>
2022-02-16 15:45:09 -0800

    Object.entries(object) converts to array

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -36,7 +36,7 @@ console.log(userEmail3.trimStart().trimEnd());
 
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 }
-
+const newUsers = Object.entries(users);
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
 
