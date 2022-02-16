8b2e76206f1eb1382a6f6255f294523bbaf3920b
samixtures <samibajwa@csu.fullerton.edu>
2022-02-16 15:48:29 -0800

    Object.fromEntires(object) converts array to object

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -44,3 +44,5 @@ newUsers.map(x => {
 })
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+const objectUsers = Object.fromEntries(newUsers);
+console.log(objectUsers);
