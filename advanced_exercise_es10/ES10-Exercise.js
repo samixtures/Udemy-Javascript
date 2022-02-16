7716dcd0206fff96ad8d26d36da30d5ddf7d7243
samixtures <samibajwa@csu.fullerton.edu>
2022-02-16 15:47:13 -0800

    tricky use of .map() method for #7

--- advanced_exercise_es10/ES10-Exercise.js
+++ advanced_exercise_es10/ES10-Exercise.js
@@ -39,6 +39,8 @@ const users = { user1: 18273, user2: 92833, user3: 90315 }
 const newUsers = Object.entries(users);
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-
+newUsers.map(x => {
+    [x[0], x[1]*2];
+})
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
